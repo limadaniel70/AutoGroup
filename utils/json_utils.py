a8bdc8dd47ea6268b93c4e2ca2a9968a5bde5bc8
Daniel Lima <dnlima77237@gmail.com>
2024-08-10 12:43:04 -0300

    Created the logic to save the data to a JSON file

--- utils/json_utils.py
+++ utils/json_utils.py
@@ -1,10 +1,12 @@
 import json
 
 
-def write_to_json_file(data: list, filename: str = "teams.json") -> None:
+def write_to_json_file(data: dict, filename: str = "teams.json") -> None:
     """
     Escreve todos os times em um arquivo json.
     :param data: Dados que serão escrito no arquivo json.
     :param filename: Nome de arquivo (padrão = teams.json).
     """
-    pass
+    with open(filename, "wt") as f:
+        json_data = json.dumps(data)
+        f.write(json_data)
