20957a57c6ec48b55d5e98a61005a0f887d83652
Daniel Lima <177990344+limadaniel70@users.noreply.github.com>
2024-08-09 23:09:01 -0300

    Removed ignored files

---
+++ config/settings.py
@@ -0,0 +1,51 @@
+# pessoas com conhecimento técnico
+hard_skill = [
+    "Vivian Rodgers",
+    "Pat Hahn",
+    "Candy Shea",
+    "Iris Platt",
+    "Walter Hanson",
+]
+
+# pessoas com habilidades em liderança ou comunicação
+soft_skill = [
+    "Pete Anthony",
+    "Brandy Brennan",
+    "Gwen Tracy",
+    "Frederick Rice",
+    "April Whitehead",
+]
+
+# pessoas com outras habilidades
+general = [
+    "Taylor Delaney",
+    "Jorge Hester",
+    "Betty Blackburn",
+    "Ron Harden",
+    "Angela Becker",
+    "Kayla Potter",
+    "Miranda Elder",
+    "Elsa Byrd",
+    "Geoffrey Lambert",
+    "Carl Hawkins",
+    "Michele McConnell",
+    "Hugh Holland",
+    "Rosa Roberts",
+    "Myra Page",
+    "Terry James",
+    "Claire Montoya",
+    "Beatrice Griffin",
+    "Micheal Gaines",
+    "Rafael Grimes",
+    "Roman Hawley",
+    "Rosemary Koch",
+    "Cameron Mills",
+    "Sally Knapp",
+    "Kenny Ewing",
+    "Dianne Shields",
+    "Nathan Shaw",
+    "Darryl Ogden",
+    "Lucille Steele",
+    "Katy McFarland",
+    "Silvia Simms"
+]
