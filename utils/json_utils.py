c28d9bfb2a2bc71f68bf3aeb8f31f281d301522f
Daniel Lima <dnlima77237@gmail.com>
2024-08-09 23:15:50 -0300

    Created json_utils.py

---
+++ utils/json_utils.py
@@ -0,0 +1,10 @@
+import json
+
+
+def write_to_json_file(data: list, filename: str = "teams.json") -> None:
+    """
+    Escreve todos os times em um arquivo json.
+    :param data: Dados que serão escrito no arquivo json.
+    :param filename: Nome de arquivo (padrão = teams.json).
+    """
+    pass
