d94a4ad00a9c57638687ed788022b74c90c0bea9
Daniel Lima <177990344+limadaniel70@users.noreply.github.com>
2024-08-10 12:43:23 -0300

    Created the logic to create teams

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 from config.settings import hard_skill, soft_skill, general
+from utils.json_utils import write_to_json_file
 
-def create_team(h_skill: list, s_skill: list) -> list:
+def create_team(h_skill: list, s_skill: list, persons_per_team: int = 6) -> list:
     """
     Cria os times baseado na quantidade de pessoas com hard skills.
     :param h_skill: Lista de pessoas com habilidades tecnicas.
-    :param s_skill: Lista de pessoas com habilidades de comunicação e de liderança
+    :param s_skill: Lista de pessoas com habilidades de comunicação e de liderança.
+    :param persons_per_team: A quantidade de pessoas em cada dia (valor padrão = 6).
     :return: Retorna uma matriz com todos os grupos criados.
     """
-    num_of_teams = len(h_skill) // 6
-    pass
+    num_of_teams: int = len(h_skill) // persons_per_team
+    all_teams = {}
+    for i, membro in enumerate(h_skill):
+        all_teams.update({f"group{i+1}":{
+            "membros":[membro]
+        }})
+
+    return  all_teams
 
 def populate_team(teams: list, general: list) -> list:
     """
@@ -19,3 +27,5 @@ def populate_team(teams: list, general: list) -> list:
     """
     pass
 
+teams = create_team(hard_skill, soft_skill)
+write_to_json_file(teams)
