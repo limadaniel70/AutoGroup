4b1cde0bc899dff5f6fee6d448fd23631c3eaf9e
Daniel Lima <177990344+limadaniel70@users.noreply.github.com>
2024-08-09 23:06:22 -0300

    Added first logic

---
+++ main.py
@@ -0,0 +1,21 @@
+from config.settings import hard_skill, soft_skill, general
+
+def create_team(h_skill: list, s_skill: list) -> list:
+    """
+    Cria os times baseado na quantidade de pessoas com hard skills.
+    :param h_skill: Lista de pessoas com habilidades tecnicas.
+    :param s_skill: Lista de pessoas com habilidades de comunicação e de liderança
+    :return: Retorna uma matriz com todos os grupos criados.
+    """
+    num_teams = len(h_skill) // 6
+    pass
+
+def populate_team(teams: list, general: list) -> list:
+    """
+    Adiciona pessoas com outras habilidades ao times criados anteriormente.
+    :param teams: Todos os times.
+    :param general: Todas as pessoas com outras habilidades.
+    :return: Retorna uma matriz contendo todos os participantes de cada time.
+    """
+    pass
+
