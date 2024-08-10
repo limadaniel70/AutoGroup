226e1f6accc833276eb8502ed2555cf86b855554
Daniel Lima <dnlima77237@gmail.com>
2024-08-10 12:55:33 -0300

    Added file indent

--- utils/json_utils.py
+++ utils/json_utils.py
@@ -5,8 +5,8 @@ def write_to_json_file(data: dict, filename: str = "teams.json") -> None:
     """
     Escreve todos os times em um arquivo json.
     :param data: Dados que serão escrito no arquivo json.
-    :param filename: Nome de arquivo (padrão = teams.json).
+    :param filename: Nome de arquivo (padrão = teams01.json).
     """
     with open(filename, "wt") as f:
-        json_data = json.dumps(data)
+        json_data = json.dumps(data, indent=4)
         f.write(json_data)
