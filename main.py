ba130827bb0a0c0466cdcdf8fda4324e78cffa05
Daniel Lima <177990344+limadaniel70@users.noreply.github.com>
2024-08-10 12:55:56 -0300

    Updated methods

--- main.py
+++ main.py
@@ -2,15 +2,12 @@ from config.settings import hard_skill, soft_skill, general
 from utils.json_utils import write_to_json_file
 
 
-def create_team(h_skill: list, persons_per_team: int = 6) -> list:
+def create_team(h_skill: list) -> list:
     """
     Cria os times baseado na quantidade de pessoas com hard skills.
     :param h_skill: Lista de pessoas com habilidades tecnicas.
-    :param s_skill: Lista de pessoas com habilidades de comunicação e de liderança.
-    :param persons_per_team: A quantidade de pessoas em cada dia (valor padrão = 6).
-    :return: Retorna uma matriz com todos os grupos criados.
+    :return: Retorna um dicionário com todos os grupos criados.
     """
-    num_of_teams: int = len(h_skill) // persons_per_team
     all_teams = {}
     for i, membro in enumerate(h_skill):
         all_teams.update({f"group{i + 1}": {
@@ -20,15 +17,16 @@ def create_team(h_skill: list, persons_per_team: int = 6) -> list:
     return all_teams
 
 
-def populate_team(teams: list, general: list) -> list:
+def populate_team(teams: dict, general: list, persons_per_team: int = 6) -> list:
     """
     Adiciona pessoas com outras habilidades ao times criados anteriormente.
     :param teams: Todos os times.
     :param general: Todas as pessoas com outras habilidades.
+    :param persons_per_team: A quantidade de pessoas em cada time (valor padrão = 6).
     :return: Retorna uma matriz contendo todos os participantes de cada time.
     """
     pass
 
 
-teams = create_team(hard_skill, soft_skill)
+teams = create_team(hard_skill)
 write_to_json_file(teams)
