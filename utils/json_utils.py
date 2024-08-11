92f7cf272af94c93518be6292006ff3a1622eba5
Daniel Lima <177990344+limadaniel70@users.noreply.github.com>
2024-08-11 14:01:15 -0300

    Fixed inconcistencies

--- utils/json_utils.py
+++ utils/json_utils.py
@@ -5,7 +5,7 @@ def write_to_json_file(data: dict, filename: str = "teams.json") -> None:
     """
     Escreve todos os times em um arquivo json.
     :param data: Dados que serão escrito no arquivo json.
-    :param filename: Nome de arquivo (padrão = teams01.json).
+    :param filename: Nome de arquivo (padrão = teams.json).
     """
     with open(filename, "wt") as f:
         json_data = json.dumps(data, indent=4)
