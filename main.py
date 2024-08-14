91c13630b3da9e3b0bf7ae1e517fd92759554de5
Daniel Lima <177990344+limadaniel70@users.noreply.github.com>
2024-08-13 21:50:50 -0300

    Updated to consider the group generator class rework

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ from config.settings import *
 from utils.group_generator import GroupGenerator
 from utils.json_utils import write_to_json_file
 
-gg = GroupGenerator()
-teams = gg.create_empty_teams(number_of_teams)
+gg = GroupGenerator(number_of_teams, hard_skill, soft_skill, general, team_size)
 write_to_json_file(
-    gg.distribute_members(teams, hard_skill, soft_skill, general, team_size)
+    gg.teams
 )
