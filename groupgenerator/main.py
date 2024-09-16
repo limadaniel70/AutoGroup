9bad4bf43852594339a8aaaa430186d5a5c98e9d
Daniel Lima <dnlima77237@gmail.com>
2024-09-16 11:21:04 -0300

    Added code documentation

--- groupgenerator/main.py
+++ groupgenerator/main.py
@@ -5,11 +5,27 @@ teams: dict[str, list[str]] = {f"equipe {x+1}": [] for x in range(N_OF_TEAMS + 1
 
 
 def add_skilled(n_teams: int, persons: list[str], teams: dict[str, list[str]]) -> None:
+    """
+    Adiciona as pessoas que possuem habilidades técnicas ou sociais.
+
+    Args:
+        n_teams (int): a quantidade de equipes.
+        persons (list[str]): as pessoas que serão adicionadas.
+        teams (dict[str, list[str]]): o dicionário com as equipes.
+    """
     for i, person in enumerate(persons):
         teams[f"equipe {i % n_teams + 1}"].append(person)
 
 
-def add_general(team_size: int, persons: list[str], teams: dict[str, list[str]]):
+def add_general(team_size: int, persons: list[str], teams: dict[str, list[str]]) -> None:
+    """
+    Adiciona as pessoas com outros tipos de habilidades.
+
+    Args:
+        team_size (int): a quantidade de pessoas em cada equipe.
+        persons (list[str]): as pessoas que serão adicionadas.
+        teams (dict[str, list[str]]): o dicionário com as equipes.
+    """
     for team in teams:
         # Em python, uma lista vazia retorna false
         # se a list tiver ao menos um lemento, ela retorna true
